e095d6842907a812cc0a98867ce3ae36e7cf5016
Сунсин Андрей Витальевич <snsn@callibri.ru>
2023-10-31 17:40:56 +0500

    выдача и поиск контактов

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,26 @@
 class ContactsController < ApplicationController
   def index
     users = []
+    limit = params[:limit] || 50
+    ofset = params[:ofset] || 0
 
-    User.find_each do |user|
+    if params[:searchParams].present?
+      users = []
+      User.where("username LIKE '%#{params[:searchParams]}%' OR
+                  first_name LIKE '%#{params[:searchParams]}%' OR
+                  last_name LIKE '%#{params[:searchParams]}%' OR
+                  email LIKE '%#{params[:searchParams]}%'")
+          .limit(limit)
+          .offset!(ofset)
+          .find_each do |user|
+            users << user.attributes
+          end
+      return render json: users
+    end
+
+    User.limit(limit).offset(ofset).find_each do |user|
+      next if user.id == @user.id
+
       users << user.attributes
     end
     render json: users
